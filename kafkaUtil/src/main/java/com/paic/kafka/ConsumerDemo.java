c35dcceab4ed219039942174131f853e65e3b1c0
endcy <endcy@qq.com>
2020-05-26 20:00:41 +0800

    demo record

--- kafkaUtil/src/main/java/com/paic/kafka/ConsumerDemo.java
+++ kafkaUtil/src/main/java/com/paic/kafka/ConsumerDemo.java
@@ -6,12 +6,17 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicLongArray;
 
 public class ConsumerDemo {
+    public static final int consumers = 4;
+    private static AtomicLongArray timeRecords = new AtomicLongArray(consumers);
+
     public static void consume() {
         //连接集群
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
+        //消费组
         props.put("group.id", "test");
 
         //以下两行代码 ---消费者自动提交offset值
@@ -23,17 +28,21 @@ public class ConsumerDemo {
         //发送数据 发送数据需要，订阅下要消费的topic。	test
         kafkaConsumer.subscribe(Arrays.asList("test"));
         long index = 0;
-        long s = 0;
         while (true) {
             ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(10);// jdk queue offer插入、poll获取元素。 blockingqueue put插入原生， take获取元素
             for (ConsumerRecord<String, String> record : consumerRecords) {
                 if (index == 0) {
-                    s = System.currentTimeMillis();
+                    long s = System.currentTimeMillis();
+                    System.out.println("consume run time:" + s);
+                    for (int i = 0; i < consumers; i++) {
+                        timeRecords.compareAndSet(i, 0, s);
+                    }
                 }
                 index++;
-//                System.out.println("get：" + record.value());
-                if (index > 999999) {
-                    System.out.println("consume 100W cost(ms): " + (System.currentTimeMillis() - s));
+                if (record.key().equals("mykey999999")) {
+                    if (timeRecords.get(0) != 0) {
+                        System.out.println("consume 100W cost(ms):" + (System.currentTimeMillis() - timeRecords.get(0)));
+                    }
                 }
             }
 
